16dc3dbb66e7691fa49603bd23f80b1a65e7596a
Ruben Romero Montes <rromerom@redhat.com>
2017-11-15 22:24:34 +0100

    added heap info for tests

--- src/test/java/com/redhat/openshift/sd/demos/FooTest.java
+++ src/test/java/com/redhat/openshift/sd/demos/FooTest.java
@@ -8,6 +8,20 @@ public class FooTest {
 
     @Test
     public void testHello() {
+
+        // Get current size of heap in bytes
+        long heapSize = Runtime.getRuntime().totalMemory();
+
+        // Get maximum size of heap in bytes. The heap cannot grow beyond this size.
+        // Any attempt will result in an OutOfMemoryException.
+        long heapMaxSize = Runtime.getRuntime().maxMemory();
+
+        // Get amount of free memory within the heap in bytes. This size will increase
+        // after garbage collection and decrease as new objects are created.
+        long heapFreeSize = Runtime.getRuntime().freeMemory();
+
+        System.out.format("Heap Size: %fMb | Max Heap: %dMb | Free Heap: %dMb", heapSize / 1024, heapMaxSize / 1024, heapFreeSize / 1024);
+
         assertEquals("Hello Kermit", new Foo().sayHello("Kermit"));
     }
 }
